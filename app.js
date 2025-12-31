49d96fb10b42cb098d9e1ca870b2e99fca72d0a2
Nguyễn Tiến Đạt <122581048+tiedat04@users.noreply.github.com>
2025-12-31 23:23:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var fut = new Date("Jan 1, 2025 00:00:00").getTime()
+var fut = new Date("Jan 1, 2026 00:00:00").getTime()
 let x = setInterval(function stime(){
     var now = new Date().getTime()
     var D = fut - now
